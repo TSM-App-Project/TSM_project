9166efa8b526cc40724888f6314edd9920a0fd0d
DavidHHH2404 <khanghuynh3467@gmail.com>
2026-06-17 16:11:48 +0700

    Bản hoàn thiện app

--- Backend/jewelry-shop-server/jewelry-shop-server/src/main/java/com/jewelry/shop/security/HashGen.java
+++ Backend/jewelry-shop-server/jewelry-shop-server/src/main/java/com/jewelry/shop/security/HashGen.java
@@ -1,4 +1,7 @@
+package com.jewelry.shop.security;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class HashGen {
     public static void main(String[] args) {
         System.out.println(new BCryptPasswordEncoder().encode("123456"));
